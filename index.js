ec419f2e79da9c27c6332d2e3742153b26a62cd8
Sajjad Asadi <lvlr.xaus@gmail.com>
2018-10-20 05:15:25 +0330

    index formatting

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import "core-js/es6/symbol";
 import "core-js/fn/symbol/iterator";
 import "./App/Config/ReactotronConfig"
 import { AppRegistry } from "react-native";
-import App from "./App/Containers/App";
+import App from "./App/Containers/App"
 
-//console.disableYellowBox = true;
+// console.disableYellowBox = true;
 const ReactNative = require("react-native");
 
 
